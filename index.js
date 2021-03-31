4a5c6028c82e5e54a07cd645ad4a3b2b5d95aee9
vincent <vincent.arteaga@gmail.com>
2021-03-30 21:32:03 -0700

    commit tree ec2be51bbd0eaf0b890ae5fe698b1d468b4a715a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM has loaded');
+    const id = document.getElementById('text');
+    id.innerHTML = 'This is really c000ool!';
+});
